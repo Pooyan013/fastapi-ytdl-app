f13da1f00f40809ce3648d0868b588f2a0413aa1
Hossein Ahmadi <Hopoah1221@gmail.com>
2025-01-17 22:09:41 +0330

    17/1

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-import os 
+import os
 from fastapi.responses import FileResponse
 from yt_dlp import YoutubeDL
+import uvicorn  # اضافه کردن uvicorn برای راه‌اندازی برنامه
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-app.mount("/static",StaticFiles(directory="static"),name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-DOWNLOAD_DIR ="downloads/"
+DOWNLOAD_DIR = "downloads/"
 
 if not os.path.exists(DOWNLOAD_DIR):
     os.makedirs(DOWNLOAD_DIR)
 
-def downlaod_video(url:str,format:str ,quality:str):
-    ydl_opts={
-        "outtmpl":f"{DOWNLOAD_DIR}%(title)s.%(ext)s",
-        "format":f"{quality}[ext={format}]"
+
+def download_video(url: str, format: str, quality: str):
+    ydl_opts = {
+        "outtmpl": f"{DOWNLOAD_DIR}%(title)s.%(ext)s",
+        "format": f"{quality}[ext={format}]"
     }
     with YoutubeDL(ydl_opts) as ydl:
-        info =ydl.extract_info(url, download=True)
+        info = ydl.extract_info(url, download=True)
         return ydl.prepare_filename(info)
 
 
@@ -30,10 +32,10 @@ async def index(request: Request):
 
 
 @app.get("/download/")
-async def download(request:Request, url ,format, quality):
+async def download(request: Request, url, format, quality):
     try:
-        file_path = downlaod_video(url, format, quality)
+        file_path = download_video(url, format, quality)
         file_name = os.path.basename(file_path)
-        return FileResponse(file_path,filename=file_name,media_type="application/octet-stream")
+        return FileResponse(file_path, filename=file_name, media_type="application/octet-stream")
     except Exception as e:
         raise HTTPException(status_code=400, detail="Error")
